31b420032370aa91810b973d0e6780465096f745
Philip Åkesson <philip.akesson@gmail.com>
2021-10-07 23:21:59 +0200

    Add more instruction implementations

--- src/Emulator.zig
+++ src/Emulator.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
+const math = std.math;
 
 const Instruction = @import("instructions.zig").Instruction;
 const decode_instruction = @import("decoder.zig").decode_instruction;
@@ -45,53 +46,212 @@ pub fn fetch_instruction(self: *const Self) u32 {
 
 pub fn execute_instruction(self: *Self, inst: Instruction) void {
     switch (inst) {
-        .add => |*rtype| {
+        .add => |*val| {
             self.setreg(
-                rtype.rd,
-                self.getreg(rtype.rs1) +% self.getreg(rtype.rs2)
+                val.rd,
+                self.getreg(val.rs1) +% self.getreg(val.rs2)
             );
         },
-        .addi => |*itype| {
+        .addi => |*val| {
             self.setreg(
-                itype.rd,
-                self.getreg(itype.rs1) +% @bitCast(u64, @intCast(i64, itype.imm))
+                val.rd,
+                self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm))
             );
         },
-        .and_ => {},
-        .andi => {},
-        .auipc => {},
-        .beq => {},
-        .bge => {},
-        .bgeu => {},
-        .blt => {},
-        .bltu => {},
-        .bne => {},
-        .jal => {},
-        .jalr => {},
-        .lb => {},
-        .lbu => {},
-        .lh => {},
-        .lhu => {},
-        .lui => {},
-        .lw => {},
-        .or_ => {},
-        .ori => {},
-        .sb => {},
-        .sh => {},
-        .sll => {},
-        .slli => {},
-        .slt => {},
-        .slti => {},
-        .sltiu => {},
-        .sltu => {},
-        .sra => {},
-        .srai => {},
-        .srl => {},
-        .srli => {},
-        .sub => {},
-        .sw => {},
-        .xor => {},
-        .xori => {},
+        .and_ => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) & self.getreg(val.rs2)
+            );
+        },
+        .andi => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) & @bitCast(u64, @intCast(i64, val.imm))
+            );
+        },
+        .auipc => |*val| {
+            self.setreg(
+                val.rd,
+                (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm))
+            );
+        },
+        .beq => |*val| {
+            if (self.getreg(val.rs1) == self.getreg(val.rs2)) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .bge => |*val| {
+            if (@bitCast(i64, self.getreg(val.rs1)) >= @bitCast(i64, self.getreg(val.rs2))) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .bgeu => |*val| {
+            if (self.getreg(val.rs1) >= self.getreg(val.rs2)) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .blt => |*val| {
+            if (@bitCast(i64, self.getreg(val.rs1)) < @bitCast(i64, self.getreg(val.rs2))) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .bltu => |*val| {
+            if (self.getreg(val.rs1) < self.getreg(val.rs2)) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .bne => |*val| {
+            if (self.getreg(val.rs1) != self.getreg(val.rs2)) {
+                // PC has already been increased
+                self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+            }
+        },
+        .jal => |*val| {
+            // TODO: instruction-address-misaligned exception if address is not aligned
+            // PC has already been increased
+            self.setreg(val.rd, self.pc);
+            self.pc = (self.pc -% 4) +% @bitCast(u64, @intCast(i64, val.imm));
+        },
+        .jalr => |*val| {
+            // TODO: instruction-address-misaligned exception if address is not aligned
+            // PC has already been increased
+            self.setreg(val.rd, self.pc);
+            self.pc = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+        },
+        .lb => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = @bitCast(u64, @intCast(i64, @bitCast(i8, self.memory.items[address])));
+            self.setreg(val.rd, value);
+        },
+        .lbu => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = @intCast(u64, self.memory.items[address]);
+            self.setreg(val.rd, value);
+        },
+        .lh => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = @bitCast(u64, @intCast(i64, @bitCast(i16, @intCast(u16, self.memory.items[address])
+                | (@intCast(u16, self.memory.items[address + 1]) << 8))));
+            self.setreg(val.rd, value);
+        },
+        .lhu => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = @intCast(u64, self.memory.items[address]) | (@intCast(u64, self.memory.items[address + 1]) << 8);
+            self.setreg(val.rd, value);
+        },
+        .lui => |*val| {
+            self.setreg(val.rd, @bitCast(u64, @intCast(i64, val.imm)));
+        },
+        .lw => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = @intCast(u64, self.memory.items[address])
+                | (@intCast(u64, self.memory.items[address + 1]) << 8)
+                | (@intCast(u64, self.memory.items[address + 2]) << 16)
+                | (@intCast(u64, self.memory.items[address + 3]) << 24);
+            self.setreg(val.rd, value);
+        },
+        .or_ => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) | self.getreg(val.rs2)
+            );
+        },
+        .ori => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) | @bitCast(u64, @intCast(i64, val.imm))
+            );
+        },
+        .sb => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            self.memory.items[address] = @truncate(u8, self.getreg(val.rs2));
+        },
+        .sh => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = self.getreg(val.rs2);
+            self.memory.items[address] = @truncate(u8, value);
+            self.memory.items[address + 1] = @truncate(u8, value >> 8);
+        },
+        .sll => |*val| {
+            const shamt = self.getreg(val.rs2) & 0b11111;
+            self.setreg(val.rd, math.shl(u64, self.getreg(val.rs1), shamt));
+        },
+        .slli => |*val| {
+            const shamt = val.imm & 0b11111;
+            self.setreg(val.rd, math.shl(u64, self.getreg(val.rs1), shamt));
+        },
+        .slt => |*val| {
+            self.setreg(
+                val.rd,
+                @boolToInt(@bitCast(i64, self.getreg(val.rs1)) < @bitCast(i64, self.getreg(val.rs2)))
+            );
+        },
+        .slti => |*val| {
+            self.setreg(
+                val.rd,
+                @boolToInt(@bitCast(i64, self.getreg(val.rs1)) < @intCast(i64, val.imm))
+            );
+        },
+        .sltiu => |*val| {
+            self.setreg(
+                val.rd,
+                @boolToInt(self.getreg(val.rs1) < @bitCast(u64, @intCast(i64, val.imm)))
+            );
+        },
+        .sltu => |*val| {
+            self.setreg(
+                val.rd,
+                @boolToInt(self.getreg(val.rs1) < self.getreg(val.rs2))
+            );
+        },
+        .sra => |*val| {
+            const shamt = self.getreg(val.rs2) & 0b11111;
+            self.setreg(val.rd, @bitCast(u64, math.shr(i64, @bitCast(i64, self.getreg(val.rs1)), shamt)));
+        },
+        .srai => |*val| {
+            const shamt = val.imm & 0b11111;
+            self.setreg(val.rd, @bitCast(u64, math.shr(i64, @bitCast(i64, self.getreg(val.rs1)), shamt)));
+        },
+        .srl => |*val| {
+            const shamt = self.getreg(val.rs2) & 0b11111;
+            self.setreg(val.rd, math.shr(u64, self.getreg(val.rs1), shamt));
+        },
+        .srli => |*val| {
+            const shamt = val.imm & 0b11111;
+            self.setreg(val.rd, math.shr(u64, self.getreg(val.rs1), shamt));
+        },
+        .sub => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) -% self.getreg(val.rs2)
+            );
+        },
+        .sw => |*val| {
+            const address = self.getreg(val.rs1) +% @bitCast(u64, @intCast(i64, val.imm));
+            const value = self.getreg(val.rs2);
+            self.memory.items[address] = @truncate(u8, value);
+            self.memory.items[address + 1] = @truncate(u8, value >> 8);
+            self.memory.items[address + 2] = @truncate(u8, value >> 16);
+            self.memory.items[address + 3] = @truncate(u8, value >> 24);
+        },
+        .xor => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) ^ self.getreg(val.rs2)
+            );
+        },
+        .xori => |*val| {
+            self.setreg(
+                val.rd,
+                self.getreg(val.rs1) ^ @bitCast(u64, @intCast(i64, val.imm))
+            );
+        },
     }
 }
 
